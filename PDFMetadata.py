901e59739294ce37ba42d5b7c944a17e9772c63e
k0rup710n <61102077+k0rup710n@users.noreply.github.com>
2020-12-17 22:40:08 -0500

    Update PDFMetadata.py

--- PDFMetadata.py
+++ PDFMetadata.py
@@ -1,22 +1,43 @@
-import pyPdf
-import optparse
-from PyPdf import PdfFileReader as readr
+import optparse, os
+from PyPDF2 import PdfFileReader as readr
+
+def pBanner():
+    os.system("clear")
+    print("""
+    ⠀⠀⠀⠀⠀⠀⠀⢠⠣⡑⡕⡱⡸⡀⡢⡂⢨⠀⡌⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠀⡕⢅⠕⢘⢜⠰⣱⢱⢱⢕⢵⠰⡱⡱⢘⡄⡎⠌⡀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠱⡸⡸⡨⢸⢸⢈⢮⡪⣣⣣⡣⡇⣫⡺⡸⡜⡎⡢⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⢱⢱⠵⢹⢸⢼⡐⡵⣝⢮⢖⢯⡪⡲⡝⠕⣝⢮⢪⢀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⢀⠂⡮⠁⠐⠀⡀⡀⠑⢝⢮⣳⣫⢳⡙⠐⠀⡠⡀⠀⠑⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⢠⠣⠐⠀ ⭕ ￼ ⠀⠀⢪⢺⣪⢣⠀⡀ ⭕     .⠈⡈⠀⡀⠀⠀ 
+⠀⠀⠀⠀⠐⡝⣕⢄⡀⠑⢙⠉⠁⡠⡣⢯⡪⣇⢇⢀⠀⠡⠁⠁⡠⡢⠡⠀⠀⠀ 
+⠀⠀⠀⠀⠀⢑⢕⢧⣣⢐⡄⣄⡍⡎⡮⣳⢽⡸⡸⡊⣧⣢⠀⣕⠜⡌⠌⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠌⡪⡪⠳⣝⢞⡆⡇⡣⡯⣞⢜⡜⡄⡧⡗⡇⠣⡃⡂⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠀⠨⢊⢜⢜⣝⣪⢪⠌⢩⢪⢃⢱⣱⢹⢪⢪⠊⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠀⠀⠐⠡⡑⠜⢎⢗⢕⢘⢜⢜⢜⠜⠕⠡⠡⡈⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠁⡢⢀⠈⠨⣂⡐⢅⢕⢐⠁⠡⠡⢁⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢈⠢⠀⡀⡐⡍⢪⢘⠀⠀⠡⡑⡀⠀⠀PARANOID⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠨⢂⠀⠌⠘⢜⠘⠀⢌⠰⡈⠀⠀⠀PDF-PARSER⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢑⢸⢌⢖⢠⢀⠪⡂
+    """)
 
 def getFileName():
     parser = optparse.OptionParser()
     parser.add_option("-f", "--file", dest="filename", help="Enter the name of your PDF File.")
     (options, argument) = parser.parse_args()
-    if not filename:
-        parser.error("Please provide a PDF file using -> -f or --file !")
+    if not options.filename:
+        parser.error("Please provide a PDF file using -f or --file !")
     else:
         return options.filename
 
 def printMeta(filename):
-    pdfFile = readr(file(filename, 'rb'))
-    docInfo = pdfFile.getDocumentInfo()
-    print("[*] PDF MetaData For : " + str(filename))
-    for metaItem in docInfo:
-        print('[+] ' + metaItem + ": " + docInfo[metaItem])
+    with open(filename, 'rb') as pdf:
+        pdfFile = readr(pdf)
+        docInfo = pdfFile.getDocumentInfo()
+        print("[*] PDF MetaData For : " + str(filename))
+        for metaItem in docInfo:
+            print('[+] ' + metaItem + ": " + docInfo[metaItem])
 
+pBanner()
 pdf = getFileName()
 printMeta(pdf)
